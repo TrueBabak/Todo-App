d36e53742b75e8610766b89718ccbe0861cb84fe
Babak <jahanibabak80@gmail.com>
2023-03-08 14:25:16 +0330

    style changes

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import AddTodo from "../AddTodo";
 import { Context } from "../context";
 import Navbar from "../Navbar";
 import TodoItemComp from "../TodoItem";
@@ -8,6 +9,7 @@ const TodoList = () => {
   return (
     <div className="w-6/12 bg-secentBg px-6 py-4 rounded-2xl shadow-md shadow-thirdBg text-textColor">
       <Navbar />
+      <AddTodo />
       <div className="grid grid-cols-2 gap-5">
         {TodoList.map((item) => {
           return (
