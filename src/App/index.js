6d677f410736b3b0d8b11ac35c98c82f8e593d74
Babak <jahanibabak80@gmail.com>
2023-03-08 03:43:15 +0330

    todo list design v.1

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { Context } from "../components/context";
+import TodoList from "../components/TodoList";
 
 const App = () => {
   const { test } = useContext(Context);
   return (
     <div className="w-full h-screen bg-mainBg flex justify-center items-center">
-      <div>App... {test}</div>
+      <TodoList />
     </div>
   );
 };
---
+++ src/components/TodoList/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div className="w-5/12 bg-secentBg px-6 py-4 rounded-2xl shadow-md shadow-thirdBg">
+      <div>TODO LIST</div>
+    </div>
+  );
+};
+
+export default TodoList;
