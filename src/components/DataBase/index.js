275c483477bd5c8dd05d1592e703486b02267c76
Babak <jahanibabak80@gmail.com>
2023-03-08 03:56:38 +0330

    database created

---
+++ src/components/DataBase/index.js
@@ -0,0 +1,26 @@
+export const TodoItem = [
+  {
+    todoName: "Portfolio",
+    status: true,
+  },
+  {
+    todoName: "Shopping App",
+    status: true,
+  },
+  {
+    todoName: "todo App",
+    status: false,
+  },
+  {
+    todoName: "weather app",
+    status: false,
+  },
+  {
+    todoName: "website",
+    status: true,
+  },
+  {
+    todoName: "blog",
+    status: false,
+  },
+];
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TodoList = () => {
   return (
-    <div className="w-5/12 bg-secentBg px-6 py-4 rounded-2xl shadow-md shadow-thirdBg">
+    <div className="w-5/12 bg-secentBg px-6 py-4 rounded-2xl shadow-md shadow-thirdBg text-textColor">
       <div>TODO LIST</div>
     </div>
   );
