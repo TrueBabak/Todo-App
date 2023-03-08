7246f58d779138ec994732f6e6c7f539558fd855
Babak <jahanibabak80@gmail.com>
2023-03-08 13:53:02 +0330

    addTodo component Added

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AddTodo from "../components/AddTodo";
 import TodoList from "../components/TodoList";
 
 const App = () => {
   return (
     <div className="w-full h-screen bg-mainBg flex justify-center items-center">
       <TodoList />
+      <AddTodo />
     </div>
   );
 };
---
+++ src/components/AddTodo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTodo = () => {
+  return (
+    <div className="absolute top-10 right-10 w-2/12 bg-secentBg rounded-xl px-6 py-4">
+      <div>Add Todo</div>
+    </div>
+  );
+};
+
+export default AddTodo;
