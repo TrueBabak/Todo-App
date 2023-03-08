4d09cefa10eb75fc8a686c497677cbf7141eb227
Babak <jahanibabak80@gmail.com>
2023-03-08 04:02:32 +0330

    usestate for todo item imported

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { TodoItem } from "../DataBase";
 
 const TodoList = () => {
   return (
     <div className="w-5/12 bg-secentBg px-6 py-4 rounded-2xl shadow-md shadow-thirdBg text-textColor">
-      <div>TODO LIST</div>
+      <div>
+        {TodoItem.map((item) => {
+          return <div>{item.todoName}</div>;
+        })}
+      </div>
     </div>
   );
 };
--- src/components/context/index.js
+++ src/components/context/index.js
@@ -2,15 +2,20 @@ import React, { createContext, useState } from "react";
 const ContextApi = {
   test: "",
   setTest: () => {},
+  TodoList: "",
+  setTodoList: () => {},
 };
 export const Context = createContext(ContextApi);
 const ContextsProvider = ({ children }) => {
   const [test, setTest] = useState("test text");
+  const [TodoList, setTodoList] = useState([]);
   return (
     <Context.Provider
       value={{
         test,
         setTest,
+        TodoList,
+        setTodoList,
       }}
     >
       {children}
