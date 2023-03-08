a6cbc91df366901f53ca9d03969a45e879b0ff38
Babak <jahanibabak80@gmail.com>
2023-03-08 15:15:54 +0330

    add todo with numpadenter key

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -6,10 +6,9 @@ const AddTodo = () => {
   return (
     <div className="bg-secentBg rounded-xl px-6 py-4">
       <div className="flex justify-center items-center mb-3">
-        Add Todo:
         <input
           type="text"
-          className="bg-thirdBg px-2 py-1 rounded-xl w-1/2 placeholder:text-mainBg mx-2"
+          className="bg-thirdBg px-2 py-1 rounded-xl w-1/2 placeholder:text-mainBg mx-2 focus:outline-double"
           maxLength="25"
           placeholder="Todo Title.."
           value={inputValue}
@@ -17,12 +16,13 @@ const AddTodo = () => {
             setInputValue(e.target.value);
           }}
           onKeyDown={(e) => {
-            if (e.code === "Enter") {
+            if (e.code === "Enter" || e.code === "NumpadEnter") {
               addTodo(inputValue);
             }
           }}
         />
         <button
+          className="px-2 py-1 bg-mainBg text-thirdBg rounded-xl border border-thirdBg shadow-md shadow-thirdBg hover:shadow-none transition-all duration-200"
           onClick={() => {
             addTodo(inputValue);
           }}
