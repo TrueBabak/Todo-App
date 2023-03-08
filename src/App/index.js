a866695b1215c389147063c6a75d497fd52e59b3
Babak <jahanibabak80@gmail.com>
2023-03-08 14:20:09 +0330

    add box

--- src/App/index.js
+++ src/App/index.js
@@ -4,7 +4,7 @@ import TodoList from "../components/TodoList";
 
 const App = () => {
   return (
-    <div className="w-full h-screen bg-mainBg flex justify-center items-center">
+    <div className="w-full h-screen bg-mainBg flex justify-start items-center pl-40">
       <TodoList />
       <AddTodo />
     </div>
--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -2,8 +2,25 @@ import React from "react";
 
 const AddTodo = () => {
   return (
-    <div className="absolute top-10 right-10 w-2/12 bg-secentBg rounded-xl px-6 py-4">
-      <div>Add Todo</div>
+    <div className="absolute top-10 right-10 w-4/12 bg-secentBg rounded-xl px-6 py-4">
+      <div className="flex justify-between items-center mb-3">
+        Add Todo:
+        <input
+          type="text"
+          className="bg-thirdBg px-2 py-1 rounded-xl w-1/2 placeholder:text-mainBg"
+          maxLength="25"
+          placeholder="Todo Title.."
+        />
+      </div>
+      <div className="flex justify-between items-center mb-3 placeholder:text-textColor">
+        Description:
+        <input
+          type="text"
+          className="bg-thirdBg px-2 py-1 rounded-xl w-1/2 placeholder:text-mainBg"
+          maxLength="25"
+          placeholder="Todo Description.."
+        />
+      </div>
     </div>
   );
 };
