89a76aebad29b3ae6d359386ee9cbab81dc4d85b
Babak <jahanibabak80@gmail.com>
2023-03-08 13:48:18 +0330

    add react icon

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 const TodoItemComp = ({ name, id, getId, removeTodo }) => {
   return (
     <div
-      onClick={() => removeTodo(id)}
-      className="border border-thirdBg shadow-md shadow-secentBg px-2 py-1 rounded-lg bg-mainBg"
+      onClick={() => getId(id)}
+      className="border border-thirdBg shadow-md shadow-secentBg px-2 py-1 rounded-lg bg-mainBg relative"
     >
+      <div
+        className="text-textColor hover:text-thirdBg absolute top-2 right-2"
+        onClick={() => {
+          removeTodo(id);
+        }}
+      >
+        <FaTimes />
+      </div>
       <div>{name}</div>
     </div>
   );
